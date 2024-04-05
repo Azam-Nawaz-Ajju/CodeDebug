865f84e66fe79b76d8b956fb51cbcd9f5504941b
Nawaz <azamnawa@amazon.com>
2024-04-05 10:35:37 +0530

    This is a print funttion with def

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+"""
+-----
+Program to print Hello World Using print and some print functionalities 
+-----
+"""
+
+def printhello()->None:
+    print("Hello World! This is great to start with...", end= ".")
+    print("Let's get started with our projects using fuctions...")
+
+printhello()
